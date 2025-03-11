cb43eb42f0429a425a434b9aea799353c413eb6c
João Rodolpho <joaorodolphocabral@gmail.com>
2025-03-11 14:38:27 -0300

    reorganization of styles

--- scripts/dunno.js
+++ scripts/dunno.js
@@ -1,32 +1,35 @@
 import { gsap } from "../node_modules/gsap/index.js";
+
 import { ScrollTrigger } from "../node_modules/gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const horizontalSections = gsap.utils.toArray('.horizontal');
+const horizontalSection = document.querySelector(".horizontal")
+let thisPinWrap = horizontalSection.querySelector('.pin-wrap');
+let thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');
 
-horizontalSections.forEach(function (sec, i) {
+let getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth);
 
-  let thisPinWrap = sec.querySelector('.pin-wrap');
-  let thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');
+gsap.fromTo(thisAnimWrap, {
+  x: () => thisAnimWrap.classList.contains('to-right') ? 0 : getToValue()
+}, {
+  x: () => thisAnimWrap.classList.contains('to-right') ? getToValue() : 0,
+  ease: "none",
+  // maxWidth: "1440px",
+  scrollTrigger: {
+    trigger: horizontalSection,
+    start: "top top",
+    end: () => -getToValue(),
+    pin: thisPinWrap,
+    // anticipatePin: 1,
+    scrub: true,
+    //markers: true,
 
-  let getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth);
+  }
+});
 
-  gsap.fromTo(thisAnimWrap, {
-    x: () => thisAnimWrap.classList.contains('to-right') ? 0 : getToValue()
-  }, {
-    x: () => thisAnimWrap.classList.contains('to-right') ? getToValue() : 0,
-    ease: "none",
-    scrollTrigger: {
-      trigger: sec,
-      start: "top top",
-      end: () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth),
-      pin: thisPinWrap,
-      invalidateOnRefresh: true,
-      //anticipatePin: 1,
-      scrub: true,
-      //markers: true,
-    }
-  });
+gsap.to(".captura", {
+  snap: {
 
+  }
 });
